b092209a52df09f5cdc54f96ed74e70dfda751cc
jungtaek kim <kgajungtaek@gmail.com>
2023-12-02 18:02:29 +0900

    docs : 객체지향프로그래밍
    프로그램 설계방법론이자 개념의 일종
    프로그램을 서로 상호작용하는 객체를 기본 단위로 구성하는 방식

---
+++ 09. OOP/Encapsulation.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._OOP
+{
+    internal class Encapsulation
+    {
+        /**********************************************************************
+         * 캡슐화 (Encapsulation)
+         *
+         * 객체를 정보와 기능으로 묶는 것을 의미
+         * 객체의 내부 정보와 기능을 숨기고, 허용한 정보와 기능만의 액세스 허용
+         **********************************************************************/
+
+        // <캡슐화>
+        // 객체를 정보와 기능으로 묶는 것, 객체의 정보와 기능을 멤버라고 표현함
+        // 현실세계의 객체를 표현하기 위해 객체가 가지는 정보와 행동을 묶어 구현하며 이를 통해 객체간 상호작용
+        class Capsule
+        {
+            int variable;           // 멤버변수 : 객체의 정보를 표현
+            void Function() { }     // 멤버함수 : 객체의 기능을 표현
+        }
+
+
+        // <접근제한자>
+        // 외부에서 접근이 가능한 멤버변수와 멤버함수를 지정하는 기능
+        // 접근제한자를 지정하지 않는 경우 기본접근제한자는 private
+        // public    : 외부에서도 접근가능
+        // private   : 내부에서만 접근가능
+        // protected : 상속한 클래스에서 public, 그외에는 private
+        class AccessSpecifier
+        {
+            public int publicValue;
+            private int privateValue;
+
+            void Function()
+            {
+                publicValue = 1;            // 접근가능
+                privateValue = 2;           // 접근가능
+            }
+        }
+
+        void Main1()
+        {
+            AccessSpecifier instance = new AccessSpecifier();
+            instance.publicValue = 3;       // 접근가능
+            // instance.privateValue = 4;   // 접근불가
+        }
+
+
+        // <정보은닉>
+        // 객체 구성에 있어서 외부에서 사용하기 원하는 기능과 사용하기 원하지 않는 기능을 구분하기 위해 사용
+        // 사용자가 객체를 사용하는데 있어서 필요한 기능만을 확인하기 위한 용도이며
+        // 외부에 의해 영향을 받지 않길 원하는 기능을 감추기 위한 용도이기도 함
+        class Bank
+        {
+            int balance;
+
+            public void Save(int money)
+            {
+                balance += money;
+            }
+
+            public void Load(int money)
+            {
+                balance -= money;
+            }
+        }
+
+        void Main2()
+        {
+            Bank bank = new Bank();
+
+            // 외부에서 Bank의 balance를 직접적으로 접근불가
+            // bank.balance = 0;
+
+            // 외부에서는 Bank에서 의도한 Save와 Load를 통해 Bank를 다루게 유도
+            bank.Save(20000);
+            bank.Load(10000);
+        }
+
+
+        // <캡슐화 사용의미 1>
+        // 캡슐화된 클래스는 외부에서 사용하기 위한 인터페이스만을 제공하여 복잡성 감소
+        // 캡슐화된 클래스는 내부적으로 어떻게 구현되었는지 몰라도 사용가능
+        class VeryComplicatedObject
+        {
+            // 캡슐화된 클래스의 private는 외부에서 접근불가하므로 사용할 수 없음
+            int veryComplicatedValue1;
+            int veryComplicatedValue2;
+            int veryComplicatedValue3;
+
+            void VeryComplicatedFunction1() { }
+            void VeryComplicatedFunction2() { }
+            void VeryComplicatedFunction3() { }
+
+            // 캡슐화된 클래스의 public은 외부에서 접근가능하므로 사용을 권장하는 기능
+            public void UseThisFunction() { }
+        }
+
+
+        // <캡슐화 사용의미 2>
+        // 캡슐화된 클래스는 외부에서 원하지 않는 사용법으로부터 보호할 수 있어 오용된 사용을 방지
+        class IntArray
+        {
+            int[] array = new int[10];
+
+            public void SetValue(int index, int value)
+            {
+                if (index < 0 || index >= 10)
+                    return;
+
+                array[index] = value;
+            }
+        }
+    }
+}
---
+++ 09. OOP/Inheritance.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._OOP
+{
+    internal class Inheritance
+    {
+        /**********************************************************************************
+         * 상속 (Inheritance)
+         *
+         * 부모클래스의 모든 기능을 가지는 자식클래스를 설계하는 방법
+         * is-a 관계 : 부모클래스가 자식클래스를 포함하는 상위개념일 경우 상속관계가 적합함
+         **********************************************************************************/
+
+        // <상속>
+        // 부모클래스를 상속하는 자식클래스에게 부모클래스의 모든 기능을 부여
+        // class 자식클래스 : 부모클래스
+        class Monster
+        {
+            protected string name;
+            protected int hp;
+
+            public void Move()
+            {
+                Console.WriteLine($"{name} 이/가 움직입니다.");
+            }
+
+            public void TakeHit(int damage)
+            {
+                hp -= damage;
+                Console.WriteLine($"{name} 이/가 {damage} 를 받아 체력이 {hp} 이 되었습니다.");
+            }
+        }
+
+        class Dragon : Monster
+        {
+            public Dragon()
+            {
+                name = "드래곤";
+                hp = 100;
+            }
+
+            public void Breath()
+            {
+                Console.WriteLine($"{name} 이/가 브레스를 뿜습니다.");
+            }
+        }
+
+        class Slime : Monster
+        {
+            public Slime()
+            {
+                name = "슬라임";
+                hp = 5;
+            }
+
+            public void Split()
+            {
+                Console.WriteLine($"{name} 이/가 분열합니다.");
+            }
+        }
+
+        class Hero
+        {
+            int damage = 3;
+            
+            public void Attack(Monster monster)
+            {
+                monster.TakeHit(damage);
+            }
+        }
+
+        void Main1()
+        {
+            Dragon dragon = new Dragon();
+            Slime slime = new Slime();
+
+            // 부모클래스 Monster를 상속한 자식클래스는 모두 부모클래스의 기능을 가지고 있음
+            dragon.Move();
+            slime.Move();
+
+            // 자식클래스는 부모클래스의 기능에 자식만의 기능을 더욱 추가하여 구현 가능
+            dragon.Breath();
+            slime.Split();
+
+            // 업캐스팅 : 자식클래스는 부모클래스 자료형으로 묵시적 형변환 가능
+            Hero hero = new Hero();
+            hero.Attack(dragon);
+            hero.Attack(slime);
+
+            Monster monster = new Dragon();
+            hero.Attack(monster);
+
+            // 다운캐스팅 : 부모클래스는 자식클래스 자료형으로 명시적 형변환 가능 (단, 가능할 경우)
+            Dragon d = (Dragon)monster;     
+            // Slime s = (Slime)monster;            // error : 인스턴스가 Slime이 아니기 때문에 변환시 오류
+
+            if (monster is Dragon)                  // 형변환이 가능한지 확인
+            {
+                Dragon isDraong = (Dragon)monster;
+            }
+
+            Dragon asDragon = monster as Dragon;    // 형변환이 가능하다면 형변환
+        }
+
+
+        // <상속 사용의미 1>
+        // 상속을 진행하는 경우 부모클래스의 소스가 자식클래스에서 모두 적용됨
+        // 부모클래스와 자식클래스의 상속관계가 적합한 경우 부모클래스에서의 기능 구현이 자식클래스에서도 어울림
+        class Building
+        {
+            // 부모클래스에서 기능을 구현할 경우 모든 부모를 상속하는 자식클래스에 기능을 구현하는 효과
+        }
+
+        class Home : Building
+        {
+            // 자식클래스에서 자식클래스만의 기능을 구현
+        }
+
+        class School : Building
+        {
+            // 자식클래스에서 자식클래스만의 기능을 구현
+        }
+
+
+        // <상속 사용의미 2>
+        // 업캐스팅을 통해 자식클래스는 부모클래스로 형변환이 가능함
+        // 자식클래스는 부모클래스를 요구하는 곳에서 동일한 기능을 수행할 수 있음
+        class Parent
+        {
+            public void Func() { }
+        }
+        class Child1 : Parent { }
+        class Child2 : Parent { }
+        class Child3 : Parent { }
+
+        void UseParent(Parent parent) { parent.Func(); }
+        void Main2()
+        {
+            Child1 child1 = new Child1();
+            Child2 child2 = new Child2();
+            Child3 child3 = new Child3();
+
+            UseParent(child1);
+            UseParent(child2);
+            UseParent(child3);
+        }
+    }
+}
