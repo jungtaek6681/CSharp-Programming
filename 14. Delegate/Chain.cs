b0236e31d4dc337df66864fd241fe149213ac5b7
jungtaek kim <kgajungtaek@gmail.com>
2023-12-24 17:49:13 +0900

    docs : 델리게이트
    대리자 : 특정 매개 변수 목록 및 반환 형식이 있는 함수에 대한 참조를 나타내는 형식

---
+++ 14. Delegate/Chain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._Delegate
+{
+    internal class Chain
+    {
+        /***************************************************
+         * 델리게이트 체인
+         * 
+         * 델리게이트 변수에 여러개의 함수를 참조하는 방법
+         ***************************************************/
+
+        // <델리게이트 체인>
+        // 하나의 델리게이트 변수에 여러 개의 함수를 할당하는 것이 가능
+        // +=, -= 연산자를 통해 할당을 추가하고 제거할 수 있음
+        // = 연산자를 통해 할당할 경우 이전의 다른 함수들을 할당한 상황이 사라짐
+        void Main()
+        {
+            Action action;
+            action = Func2;     // 델리게이트 인스턴스를 Func2 로 초기화
+            action += Func1;    // 델리게이트 인스턴스에 Func1 추가 참조
+            action += Func3;    // 델리게이트 인스턴스에 Func3 추가 참조
+            action();           // Func2, Func1, Func3 이 호출됨
+
+            action -= Func1;    // 델리게이트 인스턴스에 Func1 참조 제거
+            if (action != null) // 델리게이트 인스턴스에서 참조를 제거할 경우 참조하고 있는 함수가 없는 경우를 조심
+                action();       // Func2, Func3 이 호출됨
+
+            action += Func2;    // 같은 함수를 여러번 참조한 경우 여러번 호출됨
+            action += Func2;
+            action();           // Func2 3회, Func3 1회 호출됨
+
+            action -= Func1;    // 델리게이트 인스턴스에 참조되지 않은 함수를 제거하는 경우 해당 작업이 무시됨
+
+            action = Func1;     // 델리게이트 인스턴스에 = 을 통해 할당할 경우 이전의 참조된 상황이 사라짐
+            action();           // Func1 이 호출됨
+        }
+
+        void Func1() { Console.WriteLine("Func1"); }
+        void Func2() { Console.WriteLine("Func2"); }
+        void Func3() { Console.WriteLine("Func3"); }
+    }
+}
---
+++ 14. Delegate/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _14._Delegate
+{
+    internal class Program
+    {
+        /****************************************************************
+         * 대리자 (Delegate)
+         * 
+         * 특정 매개 변수 목록 및 반환 형식이 있는 함수에 대한 참조
+         * 대리자 인스턴스를 통해 함수를 호출 할 수 있음
+         ****************************************************************/
+
+        // <델리게이트 정의>
+        // delegate 반환형 델리게이트이름(매개변수들);
+        public delegate float DelegateMethod1(float x, float y);
+        public delegate void DelegateMethod2(string str);
+
+
+        public float Plus(float left, float right) { return left + right; }
+        public float Minus(float left, float right) { return left - right; }
+        public float Multi(float left, float right) { return left * right; }
+        public float Divide(float left, float right) { return left / right; }
+        public void Message(string message) { Console.WriteLine(message); }
+
+
+        // <델리게이트 사용>
+        // 반환형과 매개변수가 일치하는 함수를 델리게이트 변수에 할당
+        // 델리게이트 변수에 참조한 함수를 대리자를 통해 호출 가능
+        void Main1()
+        {
+            DelegateMethod1 delegate1 = new DelegateMethod1(Plus);  // 델리게이트 인스턴스 생성
+            DelegateMethod2 delegate2 = Message;                    // 간략한 문법의 델리게이트 인스턴스 생성
+
+            delegate1.Invoke(20, 10);   // Plus(20, 10);            // Invoke를 통해 참조되어 있는 함수를 호출
+            delegate2("메세지");        // Message("메세지");       // 간략한 문법의 델리게이트 함수 호출
+
+            delegate1 = Plus;
+            Console.WriteLine(delegate1(20, 10));       // output : 30
+            delegate1 = Minus;
+            Console.WriteLine(delegate1(20, 10));       // output : 10
+            delegate1 = Multi;
+            Console.WriteLine(delegate1(20, 10));       // output : 200
+            delegate1 = Divide;
+            Console.WriteLine(delegate1(20, 10));       // output : 2
+
+            // delegate2 = Plus;        // error : 반환형과 매개변수가 일치하지 않은 함수는 참조 불가
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
