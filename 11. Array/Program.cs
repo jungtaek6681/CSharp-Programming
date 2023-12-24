81f8f6d0773b2457fb3374beeba6b0bca751d3c2
jungtaek kim <kgajungtaek@gmail.com>
2023-12-24 17:24:49 +0900

    docs : 배열
    동일한 자료형의 요소들로 구성된 데이터 집합

---
+++ 11. Array/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace _11._Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*******************************************************************
+             * 배열 (Array)
+             * 
+             * 동일한 자료형의 요소들로 구성된 데이터 집합
+             * 인덱스를 통하여 배열요소(Element)에 접근할 수 있음
+             * 배열의 처음 요소의 인덱스는 0부터 시작함
+             *******************************************************************/
+
+            // <배열 기본>
+            // 배열을 만들기 위해 자료형과 크기를 정하여 생성
+            // 배열의 요소에 접근하기 위해 [인덱스] 를 사용
+            // 배열의 Length 를 통해 크기를 확인
+            // 자료형[] 배열이름 = new 자료형[크기];
+            int[] scores = new int[5];      // 크기 5의 배열 선언
+
+            scores[0] = 10;                 // 0번째 요소 저장
+            scores[1] = 20;                 // 1번째 요소 저장
+            scores[2] = 30;                 // 2번째 요소 저장
+            scores[3] = 40;                 // 3번째 요소 저장
+            scores[4] = 50;                 // 4번째 요소 저장
+
+            Console.WriteLine($"배열의 0번째 요소 : {scores[0]}");     // 0번째 요소 불러오기
+            Console.WriteLine($"배열의 1번째 요소 : {scores[1]}");     // 1번째 요소 불러오기
+            Console.WriteLine($"배열의 2번째 요소 : {scores[2]}");     // 2번째 요소 불러오기
+            Console.WriteLine($"배열의 3번째 요소 : {scores[3]}");     // 3번째 요소 불러오기
+            Console.WriteLine($"배열의 4번째 요소 : {scores[4]}");     // 4번째 요소 불러오기
+
+
+            // <배열 선언 및 초기화>
+            int[] array1;                           // 배열 변수 선언
+            array1 = new int[3];                    // 데이터를 10개 가지는 배열 생성
+            int[] array2 = new int[3] { 1, 2, 3 };  // 크기가 3인 배열을 선언하고 배열 요소들을 초기화
+            int[] array3 = new int[] { 1, 2, 3 };   // 배열의 요소들을 초기화 하는 경우 배열의 크기를 생략 가능
+            int[] array4 = { 1, 2, 3 };             // 배열의 요소들을 초기화 하는 경우 배열 생성을 생략 가능
+
+
+            // <배열의 구현 원리>
+            // C#의 배열은 Array 클래스를 통해 구현됨
+            // 따라서 Array 클래스의 모든 특징을 가짐
+            // Array 클래스의 정적 함수를 활용하여 다양한 기능 사용 가능
+            int[] array = { 1, 3, 5, 4, 2 };
+
+            int length = array.Length;   // 배열의 크기
+            int max = array.Max();       // 배열의 최대값
+            int min = array.Min();       // 배열의 최소값
+
+            Array.Sort(array);                      // 배열 정렬
+            Array.Reverse(array);                   // 배열 반전
+            int index = Array.IndexOf(array, 3);    // 배열 탐색
+
+            int[] shallow = array;                  // 배열의 얕은 복사 : 동일한 인스턴스를 참조
+            int[] deep = new int[array.Length];     // 배열의 깊은 복사 : 새로운 인스턴스를 생성하고 복사
+            Array.Copy(array, deep, array.Length);
+
+            array[0] = 0;
+            Console.WriteLine(array[0]);            // output : 0
+            Console.WriteLine(shallow[0]);          // output : 0
+            Console.WriteLine(deep[0]);             // output : 5
+
+
+            // <인덱스>
+            // 배열은 요소들을 메모리에 연속적으로 배치하는 원리로 구현
+            // 이를 이용하여 배열의 특정요소의 메모리주소를 계산할 수 있음
+            // i번째 배열요소 메모리주소 == 배열 시작 메모리주소 + (i * 자료형의 크기)
+            // 이를 인덱스라고 표현함
+
+
+            // <다차원 배열>
+            // 배열의 []괄호 안에 차원수만큼 ','를 추가
+            // 배열의 크기가 차원마다 동일함
+            int[,] matrix = new int[3, 4];          // 2차원 배열 선언
+            // [0,0] [0,1] [0,2] [0,3]
+            // [1,0] [1,1] [1,2] [1,3]
+            // [2,0] [2,1] [2,2] [2,3]
+            matrix[2, 1] = 10;                      // 2차원 배열의 y:2 x:1 배열요소 저장
+            Console.WriteLine(matrix[2, 1]);        // 2차원 배열의 y:2 x:1 배열요소 불러오기
+            Console.WriteLine(matrix.GetLength(0)); // 2차원 배열의 y 크기
+            Console.WriteLine(matrix.GetLength(1)); // 2차원 배열의 x 크기
+
+            int[,,] cube = { { {1, 2}, {3, 4} }, { {5, 6}, {7, 8} } };      // 3차원 배열 선언 및 초기화
+
+
+            // <가변 배열>
+            // 배열의 []괄호를 배열 갯수만큼 추가
+            // 배열의 크기를 각각 설정 가능
+            int[][] jagged = new int[3][];          // 배열의 갯수 설정
+            jagged[0] = new int[5];
+            jagged[1] = new int[2];
+            jagged[2] = new int[3];
+            // [0][0] [0][1] [0][2] [0][3] [0][4]
+            // [1][0] [1][1]
+            // [2][0] [2][1] [2][2]
+
+
+            // <배열과 반복>
+            // 배열의 인덱스를 반복하여 증가시키며 사용하는 경우 배열의 모든 요소를 반복 수행하는데 용이함
+            int[] ints = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(ints[i]);
+            }
+
+            int[,] tile = { { 1, 2, 3 }, { 4, 5, 6 } };
+            for (int y = 0; y < tile.GetLength(0); y++)
+            {
+                for (int x = 0; x < tile.GetLength(1); x++)
+                {
+                    Console.Write(tile[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 11. Array/StringClass.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._Array
+{
+    internal class StringClass
+    {
+        // <string>
+        // string은 문자들의 집합으로 표현됨
+        // 내부적으로 문자 배열을 통해서 구현됨
+        // 문자열은 텍스트를 나타내는 데 사용되는 char의 순차적 집합
+
+        void Main1()
+        {
+            // string 사용
+            string str = "abcde";
+            Console.WriteLine(str);     // output : abcde
+
+            // string은 char의 순차적 집합으로 표현 : 배열처럼 한글자에 접근가능
+            Console.WriteLine(str[1]);  // output : b
+            Console.WriteLine(str[3]);  // output : d
+
+            // 단, 배열식으로 접근하여 문자를 변경 불가
+            // str1[1] = 'a';           // error : 문자열의 배열접근은 읽기전용
+
+
+            // char 배열로 문자열을 표현
+            char[] array = new char[5]{ 'a', 'b', 'c', 'd', 'e' };
+            Console.WriteLine(array);
+
+            foreach (char c in array)
+            {
+                Console.Write($"{c}");
+            }
+        }
+
+
+        // <string의 불변성(Immutable)>
+        // string은 특징상 다른 기본자료형과 다르게 크기가 정해져 있지 않은 기본자료형
+        // 이유는 string은 char의 집합이므로 char의 갯수에 따라 크기가 유동적
+        // 따라서, string은 런타임 당시에 크기가 결정되며 그 크기가 일정하지 않음
+        // 이에 string은 다른 기본자료형과 다르게 구조체가 아닌 클래스로 구현되어 있음 (런타임시 크기를 정할 수 있는 메모리는 힙영역을 사용)
+        // 단, 기본자료형과 같이 값형식을 구현하기 위해 string 클래스에 처리를 값형식처럼 동작하도록 구현
+        // 이를 구현하기 위해 string 간의 대입이 있을 경우 참조에 의한 주소값 복사가 아닌 깊은 복사를 진행
+        // 결과적으로 데이터 자체를 복사하는 값형식으로 사용하지만 힙영역을 사용하기 때문에 string이 설정되면 변경할 수 없도록하는 '불변성'을 가짐
+
+        void Main2()
+        {
+            string str1 = "abced";      // 힙영역에 abced 문자열을 저장하며 이를 str이 참조함
+            str1 = "abc";               // 새로운 힙영역에 abc 문자열을 저장하며 이름 str이 참조함
+            str1 = str1 + "123";        // 새로운 힙영역에 abc123 문자열을 저장하며 이를 str이 참조함
+
+            string str2 = str1;         // class 이지만 string은 값형식처럼 사용되어야 하기 때문에
+                                        // 힙영역에 abc123 문자열을 복사하여 str2가 참조하도록 함
+        }
+
+
+        // <메모리 파편화>
+        // string 이 불변성 특징을 가지므로 새로운 데이터를 string에 할당할 때마다 기존 데이터는 버려짐
+        // 이 버려지는 힙영역의 데이터는 가비지컬렉터의 대상이 되며, 이를 반복적으로 진행할 경우 가비지컬렉터에 부담이 됨
+        // 여러 string의 할당을 반복적으로 변경하는 경우를 주의해야 프로그램의 안정성을 높일 수 있음
+        void Main3()
+        {
+            // 문자열 붙이기 연산자 사용
+            // +연산 : 권장하지 않는 방법, 가비지컬렉터에 부담이 됨
+            // "abc123def456" 문자열을 얻어내기 위해 "abc", "def", "abc123", "abc123def" 이 가비지 발생
+            string str = "abc" + 123 + "def" + 456;
+        }
+
+
+        // <String.Format>
+        // 가비지컬렉터에 부담되지 않도록 설계된 문자열 사용방법
+        // 프로그램 동작 중 각 항목이 매개변수 목록의 값으로 표현을 바꿈
+        void Main4()
+        {
+            string str = string.Format("abc{0}def{1}", 123, 456);
+        }
+
+
+        // <문자열 보간>
+        // String.Format의 간략한 표현
+        // 문자열 형식을 지정하는 더욱 읽기 쉽고 편리한 구문을 제공
+        void Main5()
+        {
+            string str = $"abc{123}def{456}";
+        }
+
+
+        // <문자열 간격 및 형식>
+        // 문자열 형식을 표현하는데 값의 간격과 형식을 정함
+        void Main6()
+        {
+            // 문자열 간격
+            Console.WriteLine($"{"ABC",-5}!");              // output : ABC  !
+            Console.WriteLine($"{"ABC",+5}!");              // output :   DEF!
+            Console.WriteLine($"{"ABC",-5}!{"DEF",+5}!");   // output : ABC  !  DEF!
+            Console.WriteLine($"{"ABC",+5}!{"DEF",-5}!");   // output :   ABC!DEF  !
+
+
+            // 문자열 형식
+            // - 10진수 형식 -
+            Console.WriteLine($"{255:D5}");                 // output : 00255
+            Console.WriteLine($"{0xFF:D5}");                // output : 00255
+
+            // - 16진수 형식 -
+            Console.WriteLine($"{255:X5}");                 // output : 000FF
+            Console.WriteLine($"{0xFF:X5}");                // output : 000FF
+
+            // - 숫자 형식 -
+            Console.WriteLine($"{123456789:N2}");           // output : 123,456,789.00
+
+            // - 고정소수점 형식 -
+            Console.WriteLine($"{0.555:F2}, {0.554:F2}");   // output : 0.56, 0.55
+
+            // - 부동소수점 형식 -
+            Console.WriteLine($"{123.4567:E3}");            // output : 1.235E+002
+
+
+            // 문자열 간격과 형식
+            Console.WriteLine($"Text:{123.456,+8:F2}");    // output : Text:  123.46
+        }
+
+
+        // <StringBuilder>
+        // 일정 버퍼를 사용하는 방식으로 가비지컬레터에 부담되지 않도록 설계됨
+        void Main7()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10; i++)
+            {
+                sb.Append((char)65 + i);
+                sb.Append(":");
+                sb.Append(65 + i);
+                sb.AppendLine();
+            }
+            string str = sb.ToString();
+        }
+    }
+}
